d179fba120b47ae421d62e749ce1d6ee6de24757
Christian Provenzano <18606244+caproven@users.noreply.github.com>
2022-09-05 18:30:25 -0400

    fix: can't deploy with config/samples
    Resolved indentation invalidating deployment specs

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1alpha1 contains API Schema definitions for the consul-kv v1alpha1 API group
-//+kubebuilder:object:generate=true
-//+groupName=consul-kv.caproven.info
+// +kubebuilder:object:generate=true
+// +groupName=consul-kv.caproven.info
 package v1alpha1
 
 import (
